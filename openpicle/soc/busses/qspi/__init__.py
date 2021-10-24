6cf4cc4aa62e79b884c54275d0374a31c9a860c4
dragonmux <git@dragonmux.network>
2021-10-24 23:37:10 +0100

    Implemented a QSPI bus interface

---
+++ openpicle/soc/busses/qspi/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: BSD-3-Clause
+#from .controller import Controller as QSPIBus
+
+# __all__ = (
+# 	'QSPIBus',
+# )
---
+++ openpicle/soc/busses/qspi/bus.py
@@ -0,0 +1,101 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from nmigen import Elaboratable, Module, Signal, Repl
+from .type import *
+
+__all__ = (
+	'Bus',
+)
+
+class Bus(Elaboratable):
+	def __init__(self, *, resource):
+		self._bus = resource
+		self.cs = Signal()
+		self.copi = Signal(8)
+		self.cipo = Signal(8)
+		self.begin = Signal()
+		self.rnw = Signal()
+		self.complete = Signal()
+
+	def elaborate(self, platform) -> Module:
+		m = Module()
+		bus = self._bus
+		data = Signal.like(self.copi)
+		bitCounter = Signal(range(8))
+		nibbleCounter = Signal(range(2))
+		write = Signal()
+		io_i = bus.io.i
+		io_o = bus.io.o
+		io_oe = bus.io.oe
+		cs = Signal()
+		copi = bus.io.o[0]
+
+		m.d.comb += [
+			self.complete.eq(0),
+			bus.cs.o.eq(self.cs | cs),
+			self.cipo.eq(data)
+		]
+
+		with m.FSM(name = 'qspi-fsm'):
+			# Begin bringup by putting the Flash into QSPI mode
+			with m.State('STARTUP'):
+				m.d.sync += [
+					cs.eq(1),
+					io_oe.eq(0b0001),
+					data.eq(SPIOpcodes.enableQSPI),
+				]
+				m.next = 'SPI-SHIFT-L'
+			with m.State('SPI-SHIFT-L'):
+				m.d.sync += [
+					bus.clk.o.eq(0),
+					copi.eq(data[7]),
+					data.eq(data.shift_left(1)),
+					bitCounter.eq(bitCounter - 1),
+				]
+				m.next = 'SPI-SHIFT-H'
+			with m.State('SPI-SHIFT-H'):
+				m.d.sync += bus.clk.o.eq(1)
+				with m.If(bitCounter == 0):
+					m.next = 'SPI-FINISH'
+				with m.Else():
+					m.next = 'SPI-SHIFT-L'
+			with m.State('SPI-FINISH'):
+				m.d.sync += [
+					cs.eq(0),
+					io_oe.eq(0b0000),
+				]
+				m.next = 'IDLE'
+
+			with m.State('IDLE'):
+				with m.If(self.begin):
+					m.d.sync += write.eq(~self.rnw)
+					m.next = 'QSPI-SHIFT-START'
+			with m.State('QSPI-SHIFT-START'):
+				m.d.sync += io_oe.eq(Repl(write, 4))
+				with m.If(write):
+					m.d.sync += data.eq(self.copi)
+				with m.Else():
+					m.d.sync += data.eq(0)
+				m.next = 'QSPI-SHIFT-L'
+			with m.State('QSPI-SHIFT-L'):
+				m.d.sync += [
+					bus.clk.o.eq(0),
+					data.eq(data.shift_left(4)),
+					nibbleCounter.eq(nibbleCounter - 1),
+				]
+				with m.If(write):
+					m.d.sync += io_o.eq(data[4:7])
+				m.next = 'QSPI-SHIFT-H'
+			with m.State('QSPI-SHIFT-H'):
+				m.d.sync += bus.clk.o.eq(1)
+				with m.If(~write):
+					m.d.sync += data[0:3].eq(io_i)
+				with m.If(nibbleCounter == 0):
+					m.d.comb += self.complete.eq(1)
+					with m.If(self.begin):
+						m.d.sync += write.eq(~self.rnw)
+						m.next = 'QSPI-SHIFT-START'
+					with m.Else():
+						m.next = 'IDLE'
+				with m.Else():
+					m.next = 'QSPI-SHIFT-L'
+		return m
