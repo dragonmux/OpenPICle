876cb68d3c218da9a799c59fcddb401a42680329
dragonmux <git@dragonmux.network>
2021-10-28 20:34:08 +0100

    Added default clock and reset resources to the platform

--- openpicle/platform.py
+++ openpicle/platform.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from nmigen.vendor.openlane import OpenLANEPlatform
+from nmigen.build import Resource, Pins, Clock, Attrs
 from nmigen_boards.resources.memory import SPIFlashResources
 
 __all__ = (
@@ -10,7 +11,10 @@ class OpenPIClePlatform(OpenLANEPlatform):
 	pdk = 'sky130A'
 	cell_library = 'sky130_fd_sc_hs'
 
-	settings = {
+	default_clk = 'clk'
+	default_rst = 'rst'
+
+	flow_settings = {
 		"PL_TARGET_DENSITY": 0.75,
 		#"FP_HORIZONTAL_HALO": 6,
 		#"FP_VERTICAL_HALO": 6,
@@ -21,6 +25,14 @@ class OpenPIClePlatform(OpenLANEPlatform):
 	}
 
 	resources = [
+		Resource('clk', 0, Pins('clk', dir = 'i', assert_width = 1),
+			Clock(100e6), Attrs()
+		),
+
+		Resource('rst', 0, Pins('rst', dir = 'i', assert_width = 1),
+			Attrs()
+		),
+
 		*SPIFlashResources(0,
 			cs_n = 'qspi_cs',
 			clk = 'qspi_clk',
@@ -28,7 +40,7 @@ class OpenPIClePlatform(OpenLANEPlatform):
 			cipo = 'qspi_io1',
 			wp_n = 'qspi_io2',
 			hold_n = 'qspi_io3'
-		)
+		),
 	]
 
 	connectors = []
