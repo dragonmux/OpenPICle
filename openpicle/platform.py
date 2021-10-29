13e4815fa787293590dbfc61735271a899fddbe4
dragonmux <git@dragonmux.network>
2021-10-29 11:15:10 +0100

    Added the power network config for the platform

--- openpicle/platform.py
+++ openpicle/platform.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from nmigen.vendor.openlane import OpenLANEPlatform
+from nmigen import Signal
 from nmigen.build import Resource, Pins, Clock, Attrs
 from nmigen_boards.resources.memory import SPIFlashResources
 
@@ -20,8 +21,12 @@ class OpenPIClePlatform(OpenLANEPlatform):
 		#"FP_VERTICAL_HALO": 6,
 		"FP_CORE_UTIL": 10,
 		# Caravel provides 2920x3520µm for activites
-		"DIE_AREA": '"0 0 2920 3520"',
+		"DIE_AREA": "0 0 2920 3520",
 		"DIODE_INSERTION_STRATEGY": 4,
+		# Caravel-required power nets:
+		"VDD_NETS": "[list {vccd1} {vccd2} {vdda1} {vdda2}]",
+		"GND_NETS": "[list {vssd1} {vssd2} {vssa1} {vssa2}]",
+		"SYNTH_USE_PG_PINS_DEFINES": "USE_POWER_PINS",
 	}
 
 	resources = [
@@ -48,6 +53,16 @@ class OpenPIClePlatform(OpenLANEPlatform):
 	def build(self, elaboratable, build_dir = 'build', do_build = True,
 		program_opts = None, do_program = False, **kwargs
 	):
+		ports = elaboratable.get_ports()
+		# This generates the power nets for the top-level even though we do nothing with them
+		for i in range(2):
+			ports.extend([
+				Signal(name = f'vccd{i}'),
+				Signal(name = f'vdda{i}'),
+				Signal(name = f'vssd{i}'),
+				Signal(name = f'vssa{i}'),
+			])
+
 		return super().build(elaboratable, name = 'user_project_wrapper', build_dir = build_dir,
 			do_build = do_build, program_opts = program_opts, do_program = do_program,
-			ports = elaboratable.get_ports(), **kwargs)
+			ports = ports, **kwargs)
